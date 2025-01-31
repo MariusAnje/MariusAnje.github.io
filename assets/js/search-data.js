67553e115fdff94de2286fcc0e905702f4c3c6db
MariusAnje <mariusanje@outlook.com>
2025-01-31 07:23:15 +0000

    Deploying to gh-pages from @ MariusAnje/MariusAnje.github.io@27774535103479875af0e77f8a0a05dbee72a6f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Go and explore the github!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -151,14 +151,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%7A%79%61%6E%32_%41%54_%7A%6A%75.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-rss',
@@ -172,14 +165,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Wd8mEEAAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
